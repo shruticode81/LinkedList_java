5a13c735a8c1659c070c1dddc3739c6243d95d39
shruti anand <vitikaawesome81@gmail.com>
2021-02-08 17:49:41 +0530

    add length method

--- LinkedList.java
+++ LinkedList.java
@@ -6,17 +6,20 @@ public class LinkedList{
 	public static Node<Integer> takeInput(){ //it returns head of the LL formed
 		Scanner s = new Scanner(System.in);
 		int data = s.nextInt();
-		Node<Integer> head = null;
-		while(data != -1){
+		Node<Integer> head = null,tail = null;
+		while(data != -2){
 			Node<Integer> newNode = new Node<Integer>(data);
 			if(head==null){
 				head = newNode;
+				tail = newNode;
 			}else{
-				Node<Integer> temp = head;
-				while(temp.next!=null){
-					temp=temp.next;
-				}
-				temp.next = newNode;
+				//Node<Integer> temp = head;
+				//while(temp.next!=null){ // it case of while time complexity is O(n^2)
+				//	temp=temp.next;
+				//}
+				//temp.next = newNode;
+				tail.next = newNode;
+				tail = tail.next; // by using tail node the time complexity is O(n)
 			}
 			data = s.nextInt();
 		}
@@ -32,8 +35,19 @@ public class LinkedList{
 		System.out.println();
 
 	}
+
+	public static int length(Node<Integer> head){
+		Node<Integer> temp = head;
+		int count=0;
+		while(temp.data!=-1){
+			count++;
+			temp = temp.next;
+		}
+		return count;
+	}
 	public static void main(String[] args){
 		Node<Integer> head=takeInput();
 		print(head);
+		System.out.println(length(head));
 	}
 }
